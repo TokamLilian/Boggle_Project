bb8dbd75054136a0bf756782c6dbfc7d6f5ca6e3
TokamLilian <tokamlilian@gmail.com>
2023-02-28 16:30:30 -0500

    backbone code with minimum required functions

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Boggle_Project
 Recreating the boggle game in python
+
+
+So, are you a boggler ?! ;)
---
+++ boggle.py
@@ -0,0 +1,30 @@
+#Sarah Jolia Enombo Ngosso
+#Ange Lilian Tchomtchoua Tokam
+
+#Boggle.py qui simulte le jeux de boggle à plusieurs joueurs
+
+
+def jouer():
+
+    return 
+
+def generer_grille(taille):
+
+    return 
+
+def est_adjacente(grille, lettre_1, lettre_2):
+
+    return 
+
+def calcul_point(grille, mots):
+
+    return 
+
+
+
+#jouer()
+#generer_grille(taille)
+#est_adjacente(grille, lettre_1, lettre_2)
+#calcul_point(grille, mots)
+
+#test()
