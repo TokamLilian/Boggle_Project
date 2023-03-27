f8b41748166d33d03fc8076e562943c83532980c
TokamLilian <tokamlilian@gmail.com>
2023-03-26 21:32:36 -0400

    finished with my personal test_ file :)

--- test_.py
+++ test_.py
@@ -1,13 +1,6 @@
 import boggle
 from boggle import *
 
-taille = 4
-#Des = generer_des(taille)
-#letter_list = dessiner_grille(taille, Des)
-#print(letter_list)
-
-letter_list = ['I', 'R', 'J', 'S', 'A', 'W', 'Z', 'Y', 'H', 'V', 'C', 'D', 'J', 'L', 'X', 'P']
-
 def test():
 
     def test_calcul_point():
@@ -25,7 +18,7 @@ def test():
     def test_max_points():
         score = ['jouer1', '13', 'joueur2', '15', 'jouer3', '13', 'joueur4', '15', 'jouer5', '13', 'joueur6', '15']
         print(max_points(score, 6))
-
+    #test_max_points()
     
     def test_dans_grille():
         grille = [ ['G', 'A', 'W'], ['H', 'G', 'D'], ['G', 'Z', 'I'] ]
@@ -40,7 +33,6 @@ def test():
     def test_dans_grille():
         grille = [ ['G', 'A', 'W'], ['H', 'G', 'H'], ['G', 'Z', 'I'] ]
         assert(dans_grille(grille, 'HH' )) == [[3,5],[3,5]], 'the indices..'
-
     #test_dans_grille()
 
     def test_dans_grille():
@@ -66,23 +58,13 @@ def test():
 
 
     def test_est_adjacente():
-        #grille = generer_grille(taille, letter_list)
-        grille = 'TYUIHG'
-        word = 'HARTA'
-        position_matrix = [ [2, 4], 14, 6, 3, 7 ]
-       
-        print(est_adjacente(grille, word))
-
-    #test_est_adjacente()
-
-    def test_est_adjacente():
         grille = [['M', 'W', 'F', 'Z'], ['J', 'M', 'Q', 'D'], ['I', 'X', 'N', 'W'], ['S', 'L', 'I', 'I']]
         word = 'ZDWI'
-        position_matrix = dans_grille(grille, word)
-        assert(position_matrix) == [3, 7, [1, 11], [8, 14, 15]]
+        #position_matrix = dans_grille(grille, word)
+        #assert(position_matrix) == [3, 7, [1, 11], [8, 14, 15]]
         assert(est_adjacente(grille, word)) == True
 
-    #test_est_adjacente()
+    test_est_adjacente()
 
     def test_est_adjacente():
         grille = [['D', 'X', 'M', 'B'], ['M', 'E', 'P', 'E'], ['J', 'W', 'H', 'T'], ['W', 'V', 'A', 'E']]
@@ -91,21 +73,18 @@ def test():
         #dessiner_grille(4, grille)
         words = ['MXME', 'MXMB']
         for word in words:
-            position_matrix = dans_grille(grille, word)
-            print(position_matrix)
+            #position_matrix = dans_grille(grille, word)
+            #print(position_matrix)
             assert(est_adjacente(grille, word)) == True, 'Check that out!!'
-            print(est_adjacente(grille, word))
+            #print(est_adjacente(grille, word))
 
-    #test_est_adjacente()
+    test_est_adjacente()
 
     def test_est_adjacente():
         grille = [['K', 'V', 'U', 'D'], ['U', 'X', 'P', 'N'], ['C', 'M', 'H', 'Q'], ['X', 'N', 'Z', 'M']]
-        word = 'KUCXNZM'                            #the x at the 5th position is considered as adjacent to C, hence it is not adjacent to N
-                                                    #we need to test instead for the x at the 12th position
-
-        #indices_matrix = [0, [2, 4], 8, [5, 12], [7, 13], 14, [9, 15]]
-        #assert(est_adjacente(grille, word)) == True, 'watch out !'
-        print(est_adjacente(grille, word))
+        word = 'KUCXNZM'      
+        #print(est_adjacente(grille, word))
+        assert(est_adjacente(grille, word)) == True
 
     test_est_adjacente()
 
@@ -114,16 +93,14 @@ def test():
         word = 'ZCZC'
         assert(est_adjacente(grille, word)) == True, 'Because two same letters reapeat themself in a line'
 
-    #test_est_adjacente()
+    test_est_adjacente()
 
     def test_est_adjacente():
         grille = [['B', 'H', 'H', 'Q'], ['F', 'Z', 'Y', 'D'], ['X', 'T', 'U', 'V'], ['K', 'J', 'Z', 'M']]
         word = 'BHHQ'
-        position_matrix = dans_grille(grille, word)
-        print(position_matrix)
         assert(est_adjacente(grille, word)) == True, 'Because two same letters are adjacent'
 
-    #test_est_adjacente()
+    test_est_adjacente()
 
     def test_est_adjacente():
         grille = [['C', 'Z', 'V', 'T', 'X'], ['S', 'P', 'A', 'B', 'U'], ['P', 'R', 'P', 'L', 'O'], ['D', 'K', 'B', 'B', 'N'], ['K', 'Z', 'N', 'M', 'C']]
@@ -131,8 +108,23 @@ def test():
         assert(est_adjacente(grille, word)) == True, 'check that'
         #print(est_adjacente(grille, word))
 
-    #test_est_adjacente()
+    test_est_adjacente()
 
+    def test_est_adjacente():
+        grille = [['O', 'V', 'C', 'L', 'F'], ['L', 'T', 'A', 'A', 'I'], ['Y', 'B', 'B', 'U', 'S'], ['H', 'O', 'I', 'A', 'L'], ['S', 'J', 'E', 'A', 'U']]
+        word = 'OTCLF'
+        assert(est_adjacente(grille, word)) == True
+        #print(est_adjacente(grille, word))
+
+    test_est_adjacente()
+
+    def test_est_adjacente():
+        grille = [['K', 'T'], ['A', 'U']]
+        word = 'AUTK'
+        assert(est_adjacente(grille, word)) == True ,'Because 0 as an index was considered as False so we take the string of adjacence'
+        #print(est_adjacente(grille, word))
+
+    test_est_adjacente()
 
     def test_get_neighboor():
         def test_coins():
@@ -197,14 +189,6 @@ def test():
         #test_milieu_ligne()
 
     #test_get_neighboor()
-
-
-    def test_est_adjacente():
-        grille = 'HAHA'
-        letter_indices = [2, 3]
-        est_adjacente(grille=grille, letter_indices=letter_indices)
-
-    #test_est_adjacente()
 
         
 test()
